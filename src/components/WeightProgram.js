ab43589a54d42a29d551c51e5614ee0e7ef3aa60
Seth Lachman <lachmanseth17@gmail.com>
2018-08-29 21:13:35 -0400

    Added first component

---
+++ src/components/WeightProgram.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class WeightProgram extends React.Component {
+    render() {
+        return <h1>Weight Training Program</h1>
+    }
+}
+
+export default WeightProgram;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import WeightProgram from './components/WeightProgram';
+
+
+render(<WeightProgram />, document.querySelector('#main'));
