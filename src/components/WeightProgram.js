b3b3e30e2265eaed703d813888dd3675af9a5c14
Seth Lachman <lachmanseth17@gmail.com>
2018-09-01 13:53:32 -0400

    Routed user to program on input

--- src/components/WeightProgram.js
+++ src/components/WeightProgram.js
@@ -2,11 +2,28 @@ import React from 'react';
 // import React, { Fragment } from 'react'; to use fragment tags, i.e. sibling detectors
 
 class WeightProgram extends React.Component {
+    myInput = React.createRef();
+
+    goToProgram = event => {
+        // 1. Stop the from from submitting
+        event.preventDefault();
+        // 2. Get the text from that input
+        const programName = this.myInput.value.value;
+        // 3. Change the page to /program/user input
+        this.props.history.push(`/program/${programName}`)
+    }
+ 
     render() {
         return (
-            <form className="weight-program">
+            <form className="weight-program" onSubmit={this.goToProgram}>
                 <h2>Please Enter a Name for your Program</h2>
-                <input type="text" required placeholder="Weight Program Name"/>
+                <input 
+                    type="text" 
+                    ref={this.myInput}
+                    required 
+                    placeholder="Program Name"
+                    
+                />
                 <button type="submit">Create Program</button>
             </form>
         )
